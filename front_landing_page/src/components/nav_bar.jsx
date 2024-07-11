772e4c62df67626e02d188f1dcfdf563b7ec5095
Rodrigo Mujica <a20190755@pucp.edu.pe>
2024-07-11 12:11:02 -0500

    feat: nav bar

--- front_landing_page/src/components/nav_bar.jsx
+++ front_landing_page/src/components/nav_bar.jsx
@@ -1,3 +1,22 @@
 import React from "react";
 import '../styles/nav_bar.css';
 
+const opciones = [
+	'Opción uno',
+	'Opción dos',
+	'Opción tres',
+	'Opción cuatro',
+	'Opción cinco',
+	'Opción seis'
+]
+    
+
+export default function NavBar() {
+	return (
+		<nav className="nav-bar">
+			{opciones.map((opcion, index) => (
+				<a href="#" key={index} className="nav-opcion">{opcion}</a>
+			))}
+		</nav>
+	);
+}
