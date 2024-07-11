75856a3d657631fc4aa2a03038b7715815f45cf6
Rodrigo Mujica <a20190755@pucp.edu.pe>
2024-07-11 10:58:50 -0500

    build: crear react app y spring boot app

---
+++ back_landing_page/src/main/java/com/solmark/back_landing_page/BackLandingPageApplication.java
@@ -0,0 +1,13 @@
+package com.solmark.back_landing_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackLandingPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackLandingPageApplication.class, args);
+	}
+
+}
---
+++ back_landing_page/src/test/java/com/solmark/back_landing_page/BackLandingPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solmark.back_landing_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackLandingPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
