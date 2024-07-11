84d44f28e068b93447fcde317a49963ed5a8e335
Rodrigo Mujica <a20190755@pucp.edu.pe>
2024-07-11 13:04:29 -0500

    feat: fila de items

--- front_landing_page/src/App.jsx
+++ front_landing_page/src/App.jsx
@@ -2,9 +2,15 @@ import { useState } from 'react'
 import './styles/App.css'
 import NavBar from './components/nav_bar'
 import comedor from './assets/comedor.jpg'
+import iconoMano from './assets/Mano.svg'
 
+const items = [
+  {id: 1, icon: iconoMano, texto: 'Lorem ipsum' },
+  {id: 2, icon: iconoMano, texto: 'Lorem ipsum' },
+  {id: 3, icon: iconoMano, texto: 'Lorem ipsum' },
+  {id: 4, icon: iconoMano, texto: 'Lorem ipsum' },
+]
 function App() {
-  
 
   return (
     <div className='body'>
@@ -17,6 +23,16 @@ function App() {
           <h1>LOGO DE INSTITUCIÓN</h1>
           <h1>FORMATO PNG</h1>
         </div>
+      </div>
+
+      {/* Fila de items */}
+      <div className="items-row">
+        {items.map((item) => (
+          <div key={item.id} className="item">
+            <img src={item.icon} alt={`Icon ${item.id}`} className="item-icon" />
+            <p className="item-text">{item.texto}</p>
+          </div>
+        ))}
       </div>
 
       
