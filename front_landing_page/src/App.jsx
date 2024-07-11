00206ee8a39e490499b41e0ad7569b3cee13fc3a
Rodrigo Mujica <a20190755@pucp.edu.pe>
2024-07-11 12:34:09 -0500

    feat: png principal logo institucion

--- front_landing_page/src/App.jsx
+++ front_landing_page/src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import './styles/App.css'
 import NavBar from './components/nav_bar'
+import comedor from './assets/comedor.jpg'
 
 function App() {
   
 
   return (
-    <>
-    <NavBar/>
+    <div className='body'>
+      {/* Barra de navegacion */}
+      <NavBar/>
+
+      {/* Imagen principal con titulo */}
+      <div className="background-container" style={{ backgroundImage: `url(${comedor})` }}>
+        <div className="overlay">
+          <h1>LOGO DE INSTITUCIÓN</h1>
+          <h1>FORMATO PNG</h1>
+        </div>
+      </div>
+
       
-    </>
+    </div>
   )
 }
 
